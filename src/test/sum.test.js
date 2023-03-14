3b2082440b42de536eb64eb17be817df3f60611c
hardikd-zignuts <hardikd@zignuts.com>
2023-03-14 10:33:06 +0530

    Matchers (toBe, toEqual, toBeUndefined, toBeNull)

---
+++ src/test/sum.test.js
@@ -0,0 +1,17 @@
+import sum from "../components/sum";
+
+test('When a & b is number', () => {
+    const result = sum('3', 2)
+
+    expect(result).toEqual(5)
+})
+
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
