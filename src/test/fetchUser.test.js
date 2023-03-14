337def6089209f8e0224b95a35b93b05fcdb8f7a
hardikd-zignuts <hardikd@zignuts.com>
2023-03-14 13:08:44 +0530

    feat: :memo: Before each

--- src/test/fetchUser.test.js
+++ src/test/fetchUser.test.js
@@ -1,11 +1,14 @@
 import fetchUsers from "../components/fetchUsers"
+let id1 = 'blank'
+beforeEach(() => {
+    console.log(id1)
+})
 
-
-test(' weather user array exist or not', async () => {
+test('weather user array exist or not', async () => {
     const data = await fetchUsers()
-    const id1 = data[0].id
+    id1 = data[0].id
     const len = data.length
-
+    console.log(id1)
     expect(id1).toEqual(1)
     expect(len).toBeGreaterThan(0)
 })
